c3db8da5a4d8544f45901e5ce6b4bf23908e952c
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-10-01 13:21:50 +0100

    Add files via upload

---
+++ Strings2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Strings2 {
+    public static void main(String args[]) {
+
+        String str;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a string:");
+        str=input.nextLine();
+        System.out.println("You entered :" + str);
+       
+
+        
+    }
+}  
